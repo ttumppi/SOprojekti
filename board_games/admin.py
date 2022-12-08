467fa215de3ca3ec5e464ec49e15173635441057
ttumppi <80556749+ttumppi@users.noreply.github.com>
2022-12-08 22:26:34 +0200

    Delete board_games directory

--- board_games/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Boardgame, Boardgamer, Passwords
-
-admin.site.register(Boardgame)
-admin.site.register(Boardgamer)
-admin.site.register(Passwords)
-# Register your models here.
